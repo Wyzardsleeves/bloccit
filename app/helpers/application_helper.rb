c02ffcab6680a460641f9b7d1efe32779cb71963
JayLatt <jlattimor3@gmail.com>
2017-04-03 05:10:21 -0400

    completed checkpoint-23-railsValidations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  #defines a method named form_group_tag which takes two arguments.
+  def form_group_tag(errors, &block)
+    css_class = 'form-group'
+    css_class << ' has-error' if errors.any?
+    #calls the content_tag helper method.
+    content_tag :div, capture(&block), class: css_class
+  end  
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,12 +6,17 @@ RSpec.describe Post, type: :model do
   let(:description){RandomData.random_paragraph}
   let(:title){RandomData.random_sentence}
   let(:body){RandomData.random_paragraph}
-  #
   let(:topic){Topic.create!(name:name, description: description)}
-  #
   let(:post){topic.posts.create!(title: title, body: body)}
 
   it{is_expected.to belong_to(:topic)}
+  # test that Post validates the presence of title, body, and topic
+  it{is_expected.to validate_presence_of(:title)}
+  it{is_expected.to validate_presence_of(:body)}
+  it{is_expected.to validate_presence_of(:topic)}
+
+  it{is_expected.to validate_length_of(:title).is_at_least(5)}
+  it{is_expected.to validate_length_of(:body).is_at_least(20)}
 
   #tests whether post has attriutes named title and body.
   describe "attributes" do
