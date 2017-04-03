c2a22a6f5c57d8898b05b9fbc22937253cb81c69
JayLatt <jlattimor3@gmail.com>
2017-04-03 07:07:58 -0400

    completed assignment-23-railsValidations

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -6,10 +6,22 @@ RSpec.describe Topic, type: :model do
   let(:public){true}  #ask Jason about this
   let(:topic){Topic.create!(name: name, description: description)}
 
+  it{should have_many(:posts)}
+  it{should validate_length_of(:name).is_at_least(5)}
+  it{should validate_length_of(:description).is_at_least(15)}
+  it{should validate_presence_of(:name)}
+  it{should validate_presence_of(:description)}
+
   # confirms that a topic responds to the appropriate attribute
   describe "attributes" do
     it "has name, description, and public attributes" do
       expect(topic).to have_attributes(name: name, description: description, public: public)
+    end
+    it "should respond to description" do
+      expect(topic).to respond_to(:description)
+    end
+    it "should respond to public" do
+      expect(topic).to respond_to(:public)
     end
     # confirms that the public attribute is set to true by default
     it "is public by default" do
