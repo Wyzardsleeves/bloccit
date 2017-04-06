1592ec747c91a7e6a7631d050420ce49a1c0f4a8
JayLatt <jlattimor3@gmail.com>
2017-04-06 11:12:34 -0400

    completed assignment-26-railsAuthenticationSigningIn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,27 @@
 module ApplicationHelper
   #defines a method named form_group_tag which takes two arguments.
   def form_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form-group'
+    end
+  end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+   end
+end
+
+=begin  Old helper contents
+module ApplicationHelper
+  #defines a method named form_group_tag which takes two arguments.
+  def form_group_tag(errors, &block)
     css_class = 'form-group'
     css_class << ' has-error' if errors.any?
     #calls the content_tag helper method.
     content_tag :div, capture(&block), class: css_class
-  end  
+  end
 end
+=end
