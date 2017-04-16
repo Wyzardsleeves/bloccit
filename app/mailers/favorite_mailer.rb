48d980f3eec0d2043ea1b74b67e5620ea85e9d85
JayLatt <jlattimor3@gmail.com>
2017-04-16 00:47:06 -0400

    completed checkpoint-31-railsFavorites

---
+++ app/mailers/favorite_mailer.rb
@@ -0,0 +1,15 @@
+class FavoriteMailer < ApplicationMailer
+  default from: "jlattimor3@gmail.com"
+
+  def new_comment(user, post, comment)
+    headers["Message-ID"] = "<comments/#{comment.id}@desolate-ocean-52857.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@desolate-ocean-52857.example>"
+    headers["References"] = "<post/#{post.id}@desolate-ocean-52857.example>"
+
+    @user = user
+    @post = post
+    @comment = comment
+
+    mail(to: user.email, subject: "New comment on #{post.title}")
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   #registers an inline callback directly after the before_save
   before_save{self.email = email.downcase if email.present?}
@@ -32,4 +33,7 @@ class User < ActiveRecord::Base
       self.name = name_array.join(" ")
     end
   end #def format_name
+  def favorite_for(post)
+    favorites.where(post_id: post.id).first
+  end  
 end
